80c79e05d6f56100bce9bebb41e26a411d47197e
ellisthomas <ellisthomas80@gmail.com>
2018-01-08 22:40:00 -0600

    styles

--- GotBlood/App_Start/BundleConfig.cs
+++ GotBlood/App_Start/BundleConfig.cs
@@ -29,7 +29,8 @@ namespace GotBlood
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/font/-awesome.min.css"));
         }
     }
 }
